4b752d94cd106c3be0cbbdc00bef846e7eefc58a
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2022-12-04 23:41:32 +0200

    shell process

--- shell.c
+++ shell.c
@@ -100,6 +100,8 @@ void ExecuteCommandS(char *arguments[1000][1000]) {
 
     int f_in = 0;
 
+    char input[64],output[64];
+
     while(iterator <= args_count)
     {//go through and execute all the commands
 
@@ -148,17 +150,72 @@ void ExecuteCommandS(char *arguments[1000][1000]) {
 
           //return 1;
 
-        } else {
+        }
 
-          dup2(f_in, 0);
+        else
+         {
+            int i = 0;
+            bool in = false , out = false;
+            while(arguments[iterator][i] !=NULL)
+              {
+               if(strcmp(arguments[iterator][i],"<")==0)
+              {        
+                argv[i]=NULL;
+                strcpy(input,argv[i+1]);
+                in=true;          
+              }               
+   
+              if(strcmp(arguments[iterator][i],">")==0)
+              {      
+                  arguments[iterator][i]=NULL;
+                  strcpy(output,argv[i+1]);
+                  out=true;
+              }         
+            }
 
-          if(iterator != args_count) {//its not the main value
+            if(in)
+            {   
 
-              dup2(fd[WRITE], 1);//write to pipe
+                // fdo is file-descriptor
+                int fd0;
+              if ((fd0 = open(input, O_RDONLY, 0)) < 0) {
+                   perror("Couldn't open input file");
+                  exit(0);
+               }           
+                // dup2() copies content of fdo in input of preceeding file
+               dup2(fd0, 0); // STDIN_FILENO here can be replaced by 0 
 
-          }
+                close(fd0); // necessary
+             
+             }
 
-          close(fd[READ]);
+    //if '>' char was found in string inputted by user 
+          else if (out)
+          {
+
+               int fd1 ;
+               if ((fd1 = creat(output , 0644)) < 0) {
+                  perror("Couldn't open the output file");
+                  exit(0);
+              }           
+
+              dup2(fd1, STDOUT_FILENO); // 1 here can be replaced by STDOUT_FILENO
+              close(fd1);
+            }
+            else
+            {
+                 dup2(f_in, 0);
+
+                 if(iterator != args_count) {//its not the main value
+
+                   dup2(fd[WRITE], 1);//write to pipe
+
+                 }
+
+                 close(fd[READ]);
+
+            }
+         
 
               exit(execvp(arguments[iterator][0], arguments[iterator]));
 
