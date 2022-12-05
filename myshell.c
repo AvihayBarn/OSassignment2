1cf36c10e464a9b9c2cb59fd895d60d12bdfa076
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2022-12-05 14:31:43 +0200

    new branch

--- myshell.c
+++ myshell.c
@@ -1,12 +1,12 @@
 // C Program to design a shell in Linux
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<readline/readline.h>
-#include<readline/history.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #define MAXCOM 1000 // max number of letters to be supported
 #define MAXLIST 100 // max number of commands to be supported
